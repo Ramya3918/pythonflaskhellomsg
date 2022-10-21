b3a1a78ec4f148a34d62ae93799dd592c286c56f
Ramya3918 <97420098+Ramya3918@users.noreply.github.com>
2022-10-21 20:23:08 +0530

    Add files via upload

--- test2.py
+++ test2.py
@@ -1,9 +1,14 @@
 from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
+import json
 app = Flask(__name__)
-@app.route('/<int:num>', methods = ['POST'])
-def disp(num):
-	return jsonify({'data': num**2})
+cors = CORS(app)
+@app.route('/', methods = ['POST'])
+def disp():
+	with open(r"\Users\rpasupulati\Desktop\ne\posts.json") as f:
+		data = json.load(f)
+	return jsonify({'data': data})
 
 if __name__ == '__main__':
-
 	app.run(debug = True)
+
